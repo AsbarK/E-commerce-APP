b0f1b696c370efeea52b8c835144050e16886bd4
asbar <asbar03KK@gmail.com>
2023-06-23 21:24:15 +0530

    created ProductListing page with some css and a category card

--- E-commerce/src/components/ProductCard.jsx
+++ E-commerce/src/components/ProductCard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 import "./ProductCard.css";
+import moniter from "../assets/th.jpg";
 
 function ProductCard(props) {
   return (
     <>
       <div id="product-card">
-        <img src="../assets/th.jpg" alt="Product Image" />
+        <img src={moniter} alt="Product Image" />
         <div id="product-card-heading">
-          <h1>{props.name}</h1>
+          <h2>{props.name}</h2>
           <h3>{props.price}</h3>
         </div>
       </div>
